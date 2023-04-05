831bf32c1a8f70904a7c0da46da7ff7594404e92
Gibeom Kim <gibkim@c2r9s6.42seoul.kr>
2023-04-05 17:11:56 +0900

    a

--- 2751.c
+++ 2751.c
@@ -1,4 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void	merge(int *nbr, int left, int mid, int right)
+{
+	int	i, j, k;
+	int	*sort;
+
+	i = left;
+	j = mid + 1;
+	k = 0;
+	sort = (int *)malloc(sizeof(int) * (right - left));
+	while (i <= mid && j <= right)
+	{
+		if (nbr[i] < nbr[j])
+			sort[k++] = nbr[i++];
+		else
+			sort[k++] = nbr[j++];
+	}
+	if (i > mid)
+	{
+		while (j <= right)
+			sort[k++] = nbr[j++];
+	}
+	else
+	{
+		while (i <= mid)
+			sort[k++] = nbr[i++];
+	}
+	k = 0;
+	while (left + k <= right)
+	{
+		nbr[left + k] = sort[k];
+		k++;
+	}
+	free(sort);
+}
+
+void	merge_sort(int	*nbr, int left, int right)
+{
+	int	mid;
+
+	if (right > left)
+	{
+		mid = (right + left) / 2;
+		merge_sort(nbr, left, mid);
+		merge_sort(nbr, mid + 1, right);
+		merge(nbr, left, mid, right);
+	}
+}
 
 int	main(void)
 {
@@ -12,8 +61,7 @@ int	main(void)
 		scanf("%d", &nbr[i]);
 		i++;
 	}
-	i = 0;
-	
+	merge_sort(nbr, 0, n - 1);
 	i = 0;
 	while (i < n)
 	{
