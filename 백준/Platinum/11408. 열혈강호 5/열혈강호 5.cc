6e89dd2826f6c04aaeadd3b93f796604aa19b8cb
gibkim <gibkim@student.42seoul.kr>
2023-10-25 16:15:49 +0900

    [Platinum III] Title: 열혈강호 5, Time: 392 ms, Memory: 7060 KB -BaekjoonHub

---
+++ 백준/Platinum/11408. 열혈강호 5/열혈강호 5.cc
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define SRC 801
+#define SINK 802
+#define INF 1000000000
+#define MIN(a, b) (a < b ? a : b)
+int N, M;
+
+class Edge
+{
+	public :
+		int target;
+		int cost;
+		int capacity;
+		int flow;
+		Edge* reverse;
+		Edge(int target, int capa, int cost)
+			: target(target), cost(cost), capacity(capa), flow(0)
+		{};
+		int remain_c()
+		{
+			return capacity - flow;
+		}
+		void push(int amount)
+		{
+			flow += amount;
+			reverse->flow -= amount;
+		}
+};
+
+std::vector < Edge* > v[810];
+
+void	add_edge(int s, int e, int capa, int cost)
+{
+	Edge *forward = new Edge(e, capa, cost);
+	Edge *reverse = new Edge(s, 0, -cost);
+	forward->reverse = reverse;
+	reverse->reverse = forward;
+	v[s].push_back(forward);
+	v[e].push_back(reverse);
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cin >> N >> M;
+	int i = 1;
+	while (i <= N)
+	{
+		add_edge(SRC, i, 1, 0);
+		i++;
+	}
+	i = 1;
+	while (i <= M)
+	{
+		add_edge(i + 400, SINK, 1, 0);
+		i++;
+	}
+	i = 1;
+	while (i <= N)
+	{
+		int n;
+		std::cin >> n;
+		int j = 0;
+		while (j < n)
+		{
+			int Mn, C;
+			std::cin >> Mn >> C;
+			add_edge(i, Mn + 400, 1, C);
+			j++;
+		}
+		i++;
+	}
+	int result = 0;
+	int cnt = 0;
+	while (1)
+	{
+		Edge *prev[810] = { NULL };
+		int dist[810];
+		bool visited[810] = { 0 };
+		std::queue < int > q;
+		std::fill(dist, dist + 810, INF);
+		dist[SRC] = 0;
+		visited[SRC] = true;
+		q.push(SRC);
+		while (!q.empty())
+		{
+			int curr = q.front();
+			q.pop();
+			visited[curr] = false;
+			i = 0;
+			int size = v[curr].size();
+			while (i < size)
+			{
+				Edge *next = v[curr][i];
+				if (next->remain_c() > 0 && dist[next->target] > dist[curr] + next->cost)
+				{
+					dist[next->target] = dist[curr] + next->cost;
+					prev[next->target] = next;
+					if (!visited[next->target])
+					{
+						q.push(next->target);
+						visited[next->target] = true;
+					}
+				}
+				i++;
+			}
+		}
+		if (prev[SINK] == NULL)
+			break ;
+		int flow = INF;
+		i = SINK;
+		while (i != SRC)
+		{
+			flow = MIN(flow, prev[i]->remain_c());
+			i = prev[i]->reverse->target;
+		}
+		i = SINK;
+		while (i != SRC)
+		{
+			result += flow * prev[i]->cost;
+			prev[i]->push(flow);
+			i = prev[i]->reverse->target;
+		}
+		cnt++;
+	}
+	std::cout << cnt << "\n" << result << "\n";
+}
