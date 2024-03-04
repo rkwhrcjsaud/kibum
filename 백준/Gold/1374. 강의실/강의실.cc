69c6b51d750dcc3b01240acc6804770238988d8c
gibkim <gibkim@student.42seoul.kr>
2024-03-04 16:34:40 +0900

    [Gold V] Title: 강의실, Time: 48 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int N;
+std::vector < std::pair< int, bool > > v;
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int num, s, e;
+		std::cin >> num >> s >> e;
+		v.push_back(std::make_pair(s, true));
+		v.push_back(std::make_pair(e, false));
+	}
+	std::sort(v.begin(), v.end());
+	int cnt = 1;
+	int max = 1;
+	for (int i = 1; i < N * 2; i++)
+	{
+		if (v[i].second)
+			cnt++;
+		else
+			cnt--;
+		if (max < cnt && (i == N * 2 - 1 || v[i].first != v[i + 1].first))
+			max = cnt;
+	}
+	std::cout << max << "\n";
+}
