631ee333fd1913c4a2db989fb19332a0631c3563
gibkim <gibkim@student.42seoul.kr>
2024-04-08 20:35:26 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,49 +1,44 @@
 #include <iostream>
+#include <vector>
 
-#define MAX(a, b) (a > b ? a : b)
-
-int n, m;
-int arr[102][2];
-int	dp[10002][102];
-
-int	solve(int max, int i)
-{
-	int j = 1;
-	while (j <= n)
-	{
-		if (i < arr[j][1])
-			dp[i][j] = dp[i][j - 1];
-		else
-			dp[i][j] = MAX(dp[i][j - 1], dp[i - arr[j][1]][j - 1] + arr[j][0]);
-		max = MAX(max, dp[i][j]);
-		j++;
-	}
-	if (max >= m)
-		return i;
-	else
-		return 0;
-}
+int N, M;
+std::vector<int> v;
+std::vector<int> w;
+int dp[101][10001];
 
 int main(void)
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cin >> n >> m;
-	int i = 1;
-	while (i <= n)
-		std::cin >> arr[i++][0];
-	i = 1;
-	while (i <= n)
-		std::cin >> arr[i++][1];
-	i = 1;
-	while (i <= 10000)
-	{
-		int ans = solve(0, i);
-		if (ans)
-		{
-			std::cout << ans << "\n";
-			break ;
-		}
-		i++;
-	}
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        int tmp;
+        std::cin >> tmp;
+        v.push_back(tmp);
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int tmp;
+        std::cin >> tmp;
+        w.push_back(tmp);
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j <= 10000; j++)
+        {
+            if (j < w[i - 1])
+                dp[i][j] = dp[i - 1][j];
+            else
+                dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + v[i - 1]);
+        }
+    }
+    for (int i = 0; i <= 10000; i++)
+    {
+        if (dp[N][i] >= M)
+        {
+            std::cout << i << '\n';
+            break ;
+        }
+    }
 }
