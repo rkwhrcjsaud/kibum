9298ec93ac22173cd07201188d2b15bb48fd0223
gibkim <gibkim@student.42seoul.kr>
2024-05-10 18:00:05 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 4 ms, Memory: 2800 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+
+int N, K, sum;
+int arr[100001];
+int dp[100001];
+
+int main(void)
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cin >> N >> K;
+    for (int i = 1; i <= N; i++)
+        std::cin >> arr[i];
+    for (int r = 1, l = 0; r <= N; r++)
+    {
+        sum += arr[r];
+        dp[r] = dp[r - 1];
+        while (sum >= K)
+        {
+            dp[r] = std::max(dp[r], dp[l] + sum - K);
+            sum -= arr[++l];
+        }
+    }
+    std::cout << dp[N] << '\n';
+}
