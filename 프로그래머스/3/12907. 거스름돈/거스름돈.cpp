6071eb24d37a19382a4470a784ee00f88a92136f
gibkim <gibkim@student.42seoul.kr>
2024-09-02 16:10:51 +0900

    [level 3] Title: 거스름돈, Time: 3.15 ms, Memory: 3.99 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[100001];
+
+int solution(int n, vector<int> money) {
+    int answer = 0;
+    dp[0] = 1;
+    for (int i = 0; i < money.size(); i++)
+    {
+        for (int j = money[i]; j <= n; j++)
+        {
+            dp[j] += dp[j - money[i]];
+        }
+    }
+    answer = dp[n];
+    return answer;
+}
